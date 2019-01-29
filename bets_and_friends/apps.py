a7576e7ffcbeb4d3d315c89fbdcb8eb44a0c33b5
arthurouzalner <panther1984@gmail.com>
2019-01-29 08:18:15 +0200

    adding a view tournaments

--- bets_and_friends/apps.py
+++ bets_and_friends/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class TournamentsConfig(AppConfig):
-    name = 'tournaments'
+# class TournamentsConfig(AppConfig):
+#     name = 'tournaments'
--- bets_and_friends/models.py
+++ bets_and_friends/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Tournament(models.Model):
-	title = models.CharField(max_length=120)
-	description = models.TextField(blank=True, null=True) # null=True -> it's ok for them to be null in the DB
-														  # blank=True -> the value can be empty
-	price = models.DecimalField(decimalPlaces=2, max_digits=1000)
-	isFeatured = models.BooleanField(default=True)
+# class Tournament(models.Model):
+# 	title = models.CharField(max_length=120)
+# 	description = models.TextField(blank=True, null=True) # null=True -> it's ok for them to be null in the DB
+# 														  # blank=True -> the value can be empty
+# 	price = models.DecimalField(decimalPlaces=2, max_digits=1000)
+# 	isFeatured = models.BooleanField(default=True)
