d568257a321ab13e5b3100f8f90f3e6b247fdc46
sirozen <sirozen9@gmail.com>
2019-02-05 21:07:03 +0200

    create League models

--- leagues/admin.py
+++ leagues/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from leagues.models import LeagueType
+from leagues.models import LeagueType,LeageStage,LeageRound,LeagueGame
 
 admin.site.register(LeagueType)
+admin.site.register(LeageStage)
+admin.site.register(LeageRound)
+admin.site.register(LeagueGame)
--- leagues/models.py
+++ leagues/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 
 class LeagueType(models.Model):
-    text = models.CharField(max_length=64)
+    league_name     = models.CharField(default="", max_length=128)
+    league_type     = models.IntegerField()
+    stage_id_list   = models.TextField(default="") # json converted - id list to string
+
+class LeageStage(models.Model):
+    stage_name          = models.CharField(max_length=128)
+    stage_start_date    = models.DateTimeField()
+    stage_end_date      = models.DateTimeField()
+    round_id_list       = models.TextField() #json converted - id list to string
+
+class LeageRound(models.Model):
+    round_number        = models.IntegerField()
+    round_start_date    = models.DateTimeField()
+    round_end_date      = models.DateTimeField()
+    game_id_list        = models.TextField() #json convered - id list to string
+
+class LeagueGame(models.Model):
+    game_date       = models.DateTimeField()
+    home_team_id    = models.IntegerField()
+    away_team_id    = models.IntegerField()
+    game_result     = models.TextField() #json converted - score to string
